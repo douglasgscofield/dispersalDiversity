6ddd2e65e2acc86a03931fd70b3aa45eb79cfb94
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-03-04 13:58:35 +0100

    refactor pmiDiversity() to use separate lists for q, q.nielsen and r

--- R/pmiDiversity.R
+++ R/pmiDiversity.R
@@ -13,191 +13,149 @@
 #'
 #' @return List of diversity calculations
 #'
-#' @examples
-#'
+# @references
+# Grivet et al 2005 (base PMI and grand means)
+# Scofield et al 2010 (pooled PMI)
+# Scofield et al. 2011 (or not?)
+# Scofield et al. 2012 Am Nat (diversity and the rest)
+#
+# @examples
+#
 #' @export pmiDiversity
 #'
 pmiDiversity <- function(tab) {
     tab <- as.matrix(tab)
-    # old code, reorders if row names are numeric
-    #if (! any(is.na(suppressWarnings(as.numeric(rownames(tab))))))
-    #  tab <- tab[order(as.numeric(rownames(tab))), ]
     G <- dim(tab)[1]
     K <- dim(tab)[2]
     N <- sum(tab)
     n.g <- apply(tab, 1, sum)
-    # PMI statistics (Grivet et al 2005 Mol Ecol, Scofield et al 2010 J Ecol)
-
-    r.gg <- numeric(G)
-    for (g in 1:G) {
-    tmp.numer <- (tab[g,]*tab[g,]) - tab[g,]
-    tmp.denom <- (n.g[g]*n.g[g]) - n.g[g]
-    r.gg[g] <- sum(tmp.numer / tmp.denom)
-    }
-    # for q.gh (same as r.gh), create table of relative genotype frequencies
     reltab <- sweep(tab, 1, n.g, FUN="/")
     Q.mat <- reltab %*% t(reltab)
-    q.gg <- diag(Q.mat) # note biased estimator is diagonal of this matrix
-    names(r.gg) <- names(q.gg)
-    q.nielsen.gg <- nielsenTransform(q.gg, n.g)
-    q.bar.0 <- sum(n.g * n.g * q.gg) / sum(n.g * n.g)
-    q.nielsen.bar.0 <- sum(n.g * n.g * q.nielsen.gg) / sum(n.g * n.g)
-    r.bar.0 <- sum((n.g*n.g*r.gg) - (n.g*r.gg)) / sum((n.g*n.g) - n.g)
+    C <- Q.mat
+    diag(C) <- 0
+    n.k <- apply(tab, 2, sum)
+    Q.k <- n.k / N
 
     # pooled PMI (Scofield et al 2010 J Ecol)
-    q.0.gh <- prop.y.0.gh <- matrix(0,G,G)
+    q.0.gh <- prop.y.0.gh <- matrix(0, G, G)
     dimnames(q.0.gh) <- dimnames(prop.y.0.gh) <- dimnames(Q.mat)
-    for (g in 1:(G-1)) {
-        for (h in (g+1):G) {
-            shared.k <- tab[g,] & tab[h,]
-            y.gh <- prop.y.0.gh[g,h] <- sum(tab[g,shared.k])
-            y.hg <- prop.y.0.gh[h,g] <- sum(tab[h,shared.k])
-            prop.y.0.gh[g,h] <- y.gh / n.g[g]
-            prop.y.0.gh[h,g] <- y.hg / n.g[h]
-            q.0.gh[g,h] <- (y.gh * y.hg)/(n.g[g]*n.g[h])
+    for (g in 1:(G - 1)) {
+        for (h in (g + 1):G) {
+            shared.k <- tab[g, ] & tab[h, ]
+            y.gh <- prop.y.0.gh[g, h] <- sum(tab[g, shared.k])
+            y.hg <- prop.y.0.gh[h, g] <- sum(tab[h, shared.k])
+            prop.y.0.gh[g, h] <- y.gh / n.g[g]
+            prop.y.0.gh[h, g] <- y.hg / n.g[h]
+            q.0.gh[g, h] <- (y.gh * y.hg)/(n.g[g] * n.g[h])
         }
     }
 
-    # Diversity measures (Scofield et al Am Nat 
-    # http://www.jstor.org/stable/10.1086/668202)
+    # Separate lists for standard calculations:
+    #     q
+    # classical bias correction:
+    #     r
+    # and Nielsen et al. 2003 bias correction:
+    #     q.nielsen
+    #
+    # Each list has the same elements
+    #
+    #    q.gg                   based on squared frequencies
+    #    q.bar.0                weighted mean of q.gg
+    #    q.unweighted.mean      unwieghted mean of q.gg
+    #    alpha.g                reciprocal of individual q.gg
+    #    alpha.unweighted.mean  mean of alpha.g
+    #    d.alpha                reciprocal of q.unweighted.mean  (TODO RENAME?)
+    #    d.gamma                reciprocal of sum of grand squared frequencies  (TODO RENAME?)
+    #    d.beta                 d.gamma / d.alpha  (TODO RENAME?)
+    #    diversity.mat          Chao pairwise off-diagonal, alpha.g on diagonal  (TODO RENAME?)
+    #    divergence.mat         diversity.mat with 0 diagonal  (TODO RENAME?)
+    #    overlap.mat            1 - divergence.mat  (TODO RENAME?)
+    #    overlap                mean overlap  (TODO RENAME?)
+    #    divergence             mean divergence  (TODO RENAME?)
 
-    # alpha
-    alpha.q <- 1/q.gg
-    alpha.q.unweighted.mean <- (1/G) * sum(alpha.q)
-    q.gg.unweighted.mean <- mean(q.gg)
-    d.alpha.q <- 1/q.gg.unweighted.mean
+    q = list()
 
-    alpha.q.nielsen <- 1/q.nielsen.gg
-    alpha.q.nielsen.unweighted.mean <- (1/G) * sum(alpha.q.nielsen)
-    q.nielsen.gg.unweighted.mean <- mean(q.nielsen.gg)
-    d.alpha.q.nielsen <- 1/q.nielsen.gg.unweighted.mean
+    q$q.gg <- diag(Q.mat)
+    q$q.bar.0 <- sum(n.g * n.g * q$q.gg) / sum(n.g * n.g)
+    q$q.unweighted.mean <- mean(q$q.gg)
+    q$alpha.g <- 1 / q$q.gg
+    q$alpha.unweighted.mean <- sum(q$alpha.g) / G
+    q$d.alpha <- 1 / q$q.unweighted.mean
+    q$d.gamma <- 1 / sum(Q.k * Q.k)
+    q$d.beta <- q$d.gamma / q$d.alpha
+    q$diversity.mat <- (2 * Q.mat) / outer(q$q.gg, q$q.gg, FUN="+")
+    diag(q$diversity.mat) <- q$alpha.g
+    q$divergence.mat <- 1 - q$diversity.mat
+    diag(q$divergence.mat) <- 0
+    q$overlap.mat <- 1 - q$divergence.mat
+    q$overlap <- sum(C) / ((G - 1) * sum(q$q.gg))
+    q$divergence <- 1 - q$overlap
 
-    alpha.r <- 1/r.gg
-    alpha.r.unweighted.mean <- (1/G) * sum(alpha.r)
-    r.gg.unweighted.mean <- mean(r.gg)
-    d.alpha.r <- 1/r.gg.unweighted.mean
+    q.nielsen = list()
 
-    # gamma
-    n.k <- apply(tab, 2, sum)
-    Q.k <- n.k / N
-    d.gamma.q <- 1/sum(Q.k * Q.k)
-    d.gamma.q.nielsen <- 1/nielsenTransform(sum(Q.k * Q.k), N)
-    d.gamma.r <- 1/sum((n.k * (n.k - 1)) / (N * (N - 1)))
+    q.nielsen$q.gg <- nielsenTransform(q.nielsen$q.gg, n.g)
+    q.nielsen$q.bar.0 <- sum(n.g * n.g * q.nielsen$q.gg) / sum(n.g * n.g)
+    q.nielsen$q.unweighted.mean <- mean(q.nielsen$q.gg)
+    q.nielsen$alpha.g <- 1 / q.nielsen$q.gg
+    q.nielsen$alpha.unweighted.mean <- sum(q.nielsen$alpha.g) / G
+    q.nielsen$d.alpha <- 1 / q.nielsen$q.unweighted.mean
+    q.nielsen$d.gamma <- 1 / nielsenTransform(sum(Q.k * Q.k), N)
+    q.nielsen$d.beta <- q.nielsen$d.gamma / q.nielsen$d.alpha
+    q.nielsen$diversity.mat <- (2 * Q.mat) / 
+                               outer(q.nielsen$q.gg, q.nielsen$q.gg, FUN="+")
+    diag(q.nielsen$diversity.mat) <- q.nielsen$alpha.g
+    q.nielsen$divergence.mat <- 1 - q.nielsen$diversity.mat
+    diag(q.nielsen$divergence.mat) <- 0
+    q.nielsen$overlap.mat <- 1 - q.nielsen$divergence.mat
+    q.nielsen$overlap <- sum(C) / ((G - 1) * sum(q.nielsen$q.gg))
+    q.nielsen$divergence <- 1 - q.nielsen$overlap
 
-    # form pairwise omega matrix, replace its diagonal with alpha
-    q.diversity.mat <- (2 * Q.mat) / outer(q.gg, q.gg, FUN="+")
-    diag(q.diversity.mat) <- alpha.q
-    #q.divergence.mat <- 1 - (2 * Q.mat) / outer(q.gg, q.gg, FUN="+")
-    q.divergence.mat <- 1 - q.diversity.mat
-    diag(q.divergence.mat) <- 0
-    q.overlap.mat <- 1 - q.divergence.mat
+    r = list()
 
-    q.nielsen.diversity.mat <- (2 * Q.mat) / 
-                             outer(q.nielsen.gg, q.nielsen.gg, FUN="+")
-    diag(q.nielsen.diversity.mat) <- alpha.q.nielsen
-    #q.nielsen.divergence.mat <- 1 - (2 * Q.mat) / outer(q.nielsen.gg, q.nielsen.gg, FUN="+")
-    q.nielsen.divergence.mat <- 1 - q.nielsen.diversity.mat
-    diag(q.nielsen.divergence.mat) <- 0
-    q.nielsen.overlap.mat <- 1 - q.nielsen.divergence.mat
-
-    r.diversity.mat <- (2 * Q.mat) / outer(r.gg, r.gg, FUN="+")
-    diag(r.diversity.mat) <- alpha.r
-    #r.divergence.mat <- 1 - (2 * Q.mat) / outer(r.gg, r.gg, FUN="+")
-    r.divergence.mat <- 1 - r.diversity.mat
-    diag(r.divergence.mat) <- 0
-    r.overlap.mat <- 1 - r.divergence.mat
-
-    ## mean overlap and divergence
-    C <- Q.mat
-    diag(C) <- 0
-
-    q.overlap <- sum(C)/((G - 1)*sum(q.gg))
-    q.divergence <- 1 - q.overlap
-
-    q.nielsen.overlap <- sum(C) / ((G - 1) * sum(q.nielsen.gg))
-    q.nielsen.divergence <- 1 - q.nielsen.overlap
-
-    r.overlap <- sum(C)/((G - 1) * sum(r.gg))
-    r.divergence <- 1 - r.overlap
+    r.gg <- numeric(G)
+    for (g in 1:G) {
+        tmp.numer <- (tab[g, ] * tab[g, ]) - tab[g, ]
+        tmp.denom <- (n.g[g] * n.g[g]) - n.g[g]
+        r.gg[g] <- sum(tmp.numer / tmp.denom)
+    }
+    names(r.gg) <- names(q$q.gg)
+    r$q.gg <- r.gg
+    r$q.bar.0 <- sum((n.g * n.g * r$q.gg) - (n.g * r$q.gg)) / 
+                 sum((n.g * n.g) - n.g)
+    r$q.unweighted.mean <- mean(r$q.gg)
+    r$alpha.g <- 1 / r$q.gg
+    r$alpha.unweighted.mean <- sum(r$alpha.g) / G
+    r$d.alpha <- 1 / r$q.unweighted.mean
+    r$d.gamma <- 1/sum((n.k * (n.k - 1)) / (N * (N - 1)))
+    r$d.beta <- r$d.gamma / r$d.alpha
+    r$diversity.mat <- (2 * Q.mat) / outer(r$q.gg, r$q.gg, FUN="+")
+    diag(r$diversity.mat) <- r$alpha.g
+    r$divergence.mat <- 1 - r$diversity.mat
+    diag(r$divergence.mat) <- 0
+    r$overlap.mat <- 1 - r$divergence.mat
+    r$overlap <- sum(C) / ((G - 1) * sum(r$q.gg))
+    r$divergence <- 1 - r$overlap
 
     # return value
     list(table = tab,   # table passed in, as a matrix
-        num.groups = G,  # number of rows (sites)
-        num.sources = K, # number of columns (sources)
-        num.samples = N, 
-        num.samples.group = n.g,
-        num.sources.group = apply(tab, 1, function(x)sum(x > 0)),
-        num.samples.source = n.k,
-        num.groups.source = apply(tab, 2, function(x)sum(x > 0)),
+         num.groups = G,  # number of rows (sites)
+         num.sources = K, # number of columns (sources)
+         num.samples = N, 
+         num.samples.group = n.g,
+         num.sources.group = apply(tab, 1, function(x) sum(x > 0)),
+         num.samples.source = n.k,
+         num.groups.source = apply(tab, 2, function(x) sum(x > 0)),
 
-        # PMI measures by group/site
-        q.gg = q.gg, 
-        q.nielsen.gg = q.nielsen.gg,
-        r.gg = r.gg, 
+         # Pooled PMI (Scofield et al 2010 J Ecol)
+         y.gh = y.gh,
+         prop.y.0.gh = prop.y.0.gh,
+         q.0.gh = q.0.gh,
 
-        # Matrix of q_gg and q_gh values
-        Q.mat = Q.mat,  # this is q.gh
+         q = q,
 
-        q.0.gh = q.0.gh,
+         r = r, 
 
-        # Pooled values (Scofield et al 2010 J Ecol)
-        y.gh = y.gh,
-        prop.y.0.gh = prop.y.0.gh,  # 
-
-        # Weighted grand means
-        q.bar.0 = q.bar.0, 
-        q.nielsen.bar.0 = q.nielsen.bar.0, 
-        r.bar.0 = r.bar.0,
-
-        # Unweighted means
-        q.gg.unweighted.mean = q.gg.unweighted.mean,
-        q.nielsen.gg.unweighted.mean = q.nielsen.gg.unweighted.mean,
-        r.gg.unweighted.mean = r.gg.unweighted.mean,
-
-        # Alpha diversities at each site
-        alpha.q = alpha.q,
-        alpha.q.nielsen = alpha.q.nielsen,
-        alpha.r = alpha.r,
-
-        # Mean alpha diversity as reciprocal of unweighted mean site PMI
-        d.alpha.q = d.alpha.q,
-        d.alpha.q.nielsen = d.alpha.q.nielsen,
-        d.alpha.r = d.alpha.r,
-
-        # Mean alpha diversity as mean of individual site alpha diversities
-        alpha.q.unweighted.mean = alpha.q.unweighted.mean,
-        alpha.q.nielsen.unweighted.mean = alpha.q.nielsen.unweighted.mean,
-        alpha.r.unweighted.mean = alpha.r.unweighted.mean,
-
-        # Gamma diversity
-        d.gamma.q = d.gamma.q,
-        d.gamma.q.nielsen = d.gamma.q.nielsen,
-        d.gamma.r = d.gamma.r,
-
-        # Beta diversity
-        d.beta.q = d.gamma.q/d.alpha.q,
-        d.beta.q.nielsen = d.gamma.q.nielsen/d.alpha.q.nielsen,
-        d.beta.r = d.gamma.r/d.alpha.r,
-
-        # Diversity matrix: q_gg on diagonal, q_gh/r_gh off-diagonal
-        # Divergence matrix: 0 diagonal, delta_gh off-diagonal
-        # You can construct the overlap matrix (1 diagonal,
-        #   omega_gh off-diagonal) via 1 - Divergence matrix
-        q.diversity.mat = q.diversity.mat,
-        q.divergence.mat = q.divergence.mat,
-        # Mean divergence and overlap
-        q.divergence = q.divergence,
-        q.overlap = q.overlap,
-
-        q.nielsen.diversity.mat = q.nielsen.diversity.mat,
-        q.nielsen.divergence.mat = q.nielsen.divergence.mat,
-        q.nielsen.divergence = q.nielsen.divergence,
-        q.nielsen.overlap = q.nielsen.overlap,
-
-        r.diversity.mat = r.diversity.mat,
-        r.divergence.mat = r.divergence.mat,
-        r.divergence = r.divergence,
-        r.overlap = r.overlap
+         q.nielsen = q.nielsen
     )
 }
 
@@ -210,11 +168,14 @@ pmiDiversity <- function(tab) {
 #'
 #' @return \code{q.gg} vector with the Nielsen et al. transform applied
 #'
+# @references TODO?????
+#
+# @examples
+#
 #' @export nielsenTransform
 #'
 nielsenTransform <- function(q.gg, n.g) {  
-    # better bias correction, Nielsen et al 2003 Mol Ecol
-    (((q.gg*(n.g+1)*(n.g-2))+(3-n.g))/((n.g-1)^2))
+    (((q.gg * (n.g+1) * (n.g - 2)) + (3 - n.g)) / ((n.g - 1)^2))
 }
 
 
